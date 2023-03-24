94c606a8d5ebfe058fbcd6762eea49916896ea14
gsuryateja2000 <surya123goodboy@gmail.com>
2023-03-24 10:29:39 +0530

    minor changes

--- javascript/result.js
+++ javascript/result.js
@@ -4,20 +4,10 @@ function yourmsg(){
     lastname1=LastName.value;
     usermail=userMail.value;
     outputMsg("Hi " + username +" "+ lastname1 + " its pleaure to meet you , we will connect  through  "+usermail+" soon!" );
+
 }
 function working(){
     alert("working");
 }
-// function userinput(){
-//     if (userName.value=null)
-//     { 
-//         outputMsg("please enter your details");
-//     }
-//     else{ 
-//        yourmsg()
-//     }
 
-
-
-// }
 SaveBtn.addEventListener('click',yourmsg);
